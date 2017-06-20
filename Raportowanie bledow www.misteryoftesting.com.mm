20555b2469a4f88fe0cac1e5ae42910d5b71915c
Ann <anna.szmyt.home@gmail.com>
2017-06-20 15:30:46 +0200

    testing files

---
+++ Raportowanie bledow www.misteryoftesting.com.mm
@@ -0,0 +1,109 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1371240409325" ID="1" MODIFIED="1371240409325" TEXT="Raportowanie b&#x142;&#x119;d&#xf3;w www.misteryoftesting.com">
+<node CREATED="1371240409325" ID="40" MODIFIED="1371240409325" POSITION="right" TEXT="Zyskanie wiarygodno&#x15b;ci testera ">
+<node CREATED="1371240409325" ID="41" MODIFIED="1371240409325" TEXT="Ka&#x17c;dego dnia tw&#xf3;rz najlepsze raporty b&#x142;&#x119;d&#xf3;w."/>
+<node CREATED="1371240409325" ID="42" MODIFIED="1371240409325" TEXT="Zosta&#x144; ekspertem"/>
+<node CREATED="1371240409325" ID="43" MODIFIED="1371240409325" TEXT="Zosta&#x144; profesjonalist&#x105;"/>
+<node CREATED="1371240409325" ID="44" MODIFIED="1371240409325" TEXT="Zdobywaj przyja&#x17a;nie"/>
+<node CREATED="1371240409325" ID="45" MODIFIED="1371240409325" TEXT="Nie marnuj czyjego&#x15b; czasu "/>
+</node>
+<node CREATED="1371240409325" ID="7" MODIFIED="1371240409325" POSITION="right" TEXT="Sprzedaj to!">
+<node CREATED="1371240409326" ID="8" MODIFIED="1371240409326" TEXT="Dla kogo jest to wa&#x17c;ne?">
+<node CREATED="1371240409326" ID="9" MODIFIED="1371240409326" TEXT="Zesp&#xf3;&#x142; programistyczny">
+<node CREATED="1371240409326" ID="10" MODIFIED="1371240409326" TEXT="B&#x105;d&#x17a; przygotowany do zarzut&#xf3;w">
+<node CREATED="1371240409326" ID="11" MODIFIED="1371240409326" TEXT="Czy to stary b&#x142;&#x105;d ?"/>
+<node CREATED="1371240409326" ID="12" MODIFIED="1371240409326" TEXT="Czy jest to wa&#x17c;ne dla Biznesu ?"/>
+<node CREATED="1371240409326" ID="13" MODIFIED="1371240409326" TEXT="Komu zale&#x17c;y na tym? Nie jest to wa&#x17c;ne."/>
+<node CREATED="1371240409326" ID="14" MODIFIED="1371240409326" TEXT="Raport b&#x142;&#x119;d&#xf3;w nie jest jasny"/>
+<node CREATED="1371240409326" ID="15" MODIFIED="1371240409326" TEXT="Zbyt du&#x17c;e koszty naprawy"/>
+<node CREATED="1371240409326" ID="16" MODIFIED="1371240409326" TEXT="Nie oddzia&#x142;uje na klient&#xf3;w"/>
+<node CREATED="1371240409326" ID="17" MODIFIED="1371240409326" TEXT="Niemo&#x17c;liwe do odtworzenia"/>
+<node CREATED="1371240409326" ID="18" MODIFIED="1371240409326" TEXT="Trudne do odtworzenia"/>
+<node CREATED="1371240409327" ID="19" MODIFIED="1371240409327" TEXT="Czy tester podtrzymuje zaufanie ?"/>
+</node>
+<node CREATED="1371240409327" ID="20" MODIFIED="1371240409327" TEXT="Dlaczego oni chcieliby naprawi&#x107; b&#x142;&#x105;d?">
+<node CREATED="1371240409327" ID="21" MODIFIED="1371240409327" TEXT="Konsekwencje">
+<node CREATED="1371240409327" ID="32" MODIFIED="1371240409327" TEXT="Wygl&#x105;da &#x17a;le">
+<node CREATED="1371240409327" ID="33" MODIFIED="1371240409327" TEXT="Spadek reputacji firmy"/>
+<node CREATED="1371240409327" ID="34" MODIFIED="1371240409327" TEXT="Jest &#x17c;enuj&#x105;ce"/>
+<node CREATED="1371240409327" ID="88" MODIFIED="1371240409327" TEXT="Z&#x142;y spos&#xf3;b przedstawienia w mediach i &#x15b;rodowisku "/>
+</node>
+<node CREATED="1371240409327" ID="28" MODIFIED="1371240409327" TEXT="Finansowe">
+<node CREATED="1371240409327" ID="30" MODIFIED="1371240409327" TEXT="Oszcz&#x119;dno&#x15b;&#x107; pieni&#x119;dzy"/>
+<node CREATED="1371240409327" ID="31" MODIFIED="1371240409327" TEXT="Kosztuje to biznes wi&#x119;cej pieni&#x119;dzy"/>
+</node>
+</node>
+<node CREATED="1371240409327" ID="22" MODIFIED="1371240409327" TEXT="Mog&#x142;o by&#x107; &#x142;atwe do naprawy"/>
+<node CREATED="1371240409327" ID="23" MODIFIED="1371240409327" TEXT="Wierz&#x105; w twoj&#x105; zdolno&#x15b;&#x107;  umiej&#x119;tno&#x15b;&#x107; oceniania "/>
+<node CREATED="1371240409327" ID="24" MODIFIED="1371240409327" TEXT="Wa&#x17c;no&#x15b;&#x107;...">
+<node CREATED="1371240409327" ID="25" MODIFIED="1371240409327" TEXT="B&#x119;dzie mia&#x142;o wp&#x142;yw na wielu ludzi"/>
+<node CREATED="1371240409328" ID="26" MODIFIED="1371240409328" TEXT="Biznes chcia&#x142;by mie&#x107; to naprawione"/>
+<node CREATED="1371240409328" ID="27" MODIFIED="1371240409328" TEXT="Jest relatywnie &#x142;atwe do znalezienia"/>
+</node>
+</node>
+</node>
+<node CREATED="1371240409328" ID="35" MODIFIED="1371240409328" TEXT="Na ile b&#x142;&#x105;d jest istotny dla..">
+<node CREATED="1371240409328" ID="36" MODIFIED="1371240409328" TEXT="U&#x17c;ytkownika"/>
+<node CREATED="1371240409328" ID="37" MODIFIED="1371240409328" TEXT="Interesariuszy"/>
+<node CREATED="1371240409328" ID="38" MODIFIED="1371240409328" TEXT="Prasy"/>
+<node CREATED="1371240409328" ID="39" MODIFIED="1371240409328" TEXT="Spo&#x142;eczno&#x15b;ci"/>
+</node>
+</node>
+</node>
+<node CREATED="1371240409328" ID="47" MODIFIED="1371240409328" POSITION="right" TEXT="Cechy dobrego raportu">
+<node CREATED="1371240409328" ID="48" MODIFIED="1371240409328" TEXT="Zawarto&#x15b;&#x107;">
+<node CREATED="1371240409328" ID="49" MODIFIED="1371240409328" TEXT="Jak reprodukowa&#x107;">
+<node CREATED="1371240409328" ID="50" MODIFIED="1371240409328" TEXT="Wersja / numer produktu"/>
+<node CREATED="1371240409328" ID="51" MODIFIED="1371240409328" TEXT="Sprawd&#x17a; &#x15a;rodowisko"/>
+<node CREATED="1371240409329" ID="52" MODIFIED="1371240409329" TEXT="Wyja&#x15b;nij kroki"/>
+<node CREATED="1371240409329" ID="53" MODIFIED="1371240427355" TEXT="Upro&#x15b;&#x107;"/>
+</node>
+<node CREATED="1371240409329" ID="54" MODIFIED="1371240409329" TEXT="Za&#x142;&#x105;czniki">
+<node CREATED="1371240409329" ID="55" MODIFIED="1371240409329" TEXT="Zrzuty ekranu"/>
+<node CREATED="1371240409329" ID="56" MODIFIED="1371240409329" TEXT="Filmy"/>
+<node CREATED="1371240409329" ID="57" MODIFIED="1371240409329" TEXT="Dane"/>
+<node CREATED="1371240409329" ID="58" MODIFIED="1371240409329" TEXT="Skrypty"/>
+</node>
+<node CREATED="1371240409329" ID="59" MODIFIED="1371240409329" TEXT="Upewnij si&#x119;, &#x17c;e to nie jest duplikat"/>
+<node CREATED="1371240409329" ID="60" MODIFIED="1371240409329" TEXT="Spos&#xf3;b opisu">
+<node CREATED="1371240409329" ID="61" MODIFIED="1371240409329" TEXT="Nag&#x142;&#xf3;wki / tytu&#x142;y"/>
+<node CREATED="1371240409329" ID="62" MODIFIED="1371240409329" TEXT="U&#x17c;ywanie filtr&#xf3;w i s&#x142;&#xf3;w kluczowych"/>
+<node CREATED="1371240409329" ID="63" MODIFIED="1371240409329" TEXT="Przekazanie b&#x142;&#x119;d&#xf3;w"/>
+<node CREATED="1371240409329" ID="64" MODIFIED="1371240409329" TEXT="Neutralny ton"/>
+</node>
+</node>
+<node CREATED="1371240409330" ID="65" MODIFIED="1371240409330" TEXT="Techniki">
+<node CREATED="1371240409330" ID="66" MODIFIED="1371240409330" TEXT="Replikowanie">
+<node CREATED="1371240409330" ID="67" MODIFIED="1371240409330" TEXT="Czy &#x142;atwo replikowa&#x107; na podstawie informacji od dostawcy?"/>
+<node CREATED="1371240409330" ID="68" MODIFIED="1371240409330" TEXT="Nie odtwarzalne?">
+<node CREATED="1371240409330" ID="69" MODIFIED="1371240409330" TEXT="Zanotuj tak du&#x17c;o informacji jak to tylko mo&#x17c;liwe"/>
+<node CREATED="1371240409330" ID="70" MODIFIED="1371240409330" TEXT="Mo&#x17c;e zajmowa&#x107; zbyt du&#x17c;o czasu i by&#x107; zbyt kosztowne"/>
+</node>
+</node>
+<node CREATED="1371240409330" ID="71" MODIFIED="1371240409330" TEXT="Uog&#xf3;lnianie">
+<node CREATED="1371240409330" ID="72" MODIFIED="1371240409330" TEXT="W jaki spos&#xf3;b &#x142;atwo oddzia&#x142;uje na u&#x17c;ytkownik&#xf3;w"/>
+</node>
+<node CREATED="1371240409330" ID="73" MODIFIED="1371240409330" TEXT="Wyszczeg&#xf3;lnianie">
+<node CREATED="1371240409330" ID="74" MODIFIED="1371240409330" TEXT="Jaka jest najprostsza droga do reprodukcji b&#x142;&#x119;du?"/>
+<node CREATED="1371240409330" ID="75" MODIFIED="1371240409330" TEXT="Upro&#x15b;ci&#x107;">
+<node CREATED="1371240409330" ID="76" MODIFIED="1371240409330" TEXT="Jeden b&#x142;&#x105;d na zdarzenie"/>
+<node CREATED="1371240409330" ID="77" MODIFIED="1371240409330" TEXT="Dodaj dodatkowe notatki"/>
+<node CREATED="1371240409331" ID="78" MODIFIED="1371240409331" TEXT="Usu&#x144; niepotrzebne kroki"/>
+</node>
+<node CREATED="1371240409331" ID="79" MODIFIED="1371240409331" TEXT="Prze&#x15b;led&#x17a; dok&#x142;adnie powody b&#x142;&#x119;du"/>
+</node>
+<node CREATED="1371240409331" ID="80" MODIFIED="1371240409331" TEXT="Maksymalizowanie">
+<node CREATED="1371240409331" ID="81" MODIFIED="1371240409331" TEXT="Na ile jest to powa&#x17c;ne ?"/>
+<node CREATED="1371240409331" ID="82" MODIFIED="1371240409331" TEXT="Co jest najgorszym niepowodzeniem jakie mo&#x17c;esz reprodukowa&#x107;?"/>
+<node CREATED="1371240409331" ID="83" MODIFIED="1371240409331" TEXT="Czy mo&#x17c;esz zmieni&#x107; to na b&#x142;&#x105;d krytyczny ?"/>
+<node CREATED="1371240409331" ID="84" MODIFIED="1371240409331" TEXT="Spr&#xf3;buj dalsze testy , co si&#x119; wydarzy je&#x15b;li...">
+<node CREATED="1371240409331" ID="85" MODIFIED="1371240409331" TEXT="Skonfigurujesz ustawienia oprogramowania"/>
+<node CREATED="1371240409331" ID="86" MODIFIED="1371240409331" TEXT="Zmienisz &#x15b;rodowisko testowe"/>
+<node CREATED="1371240409331" ID="87" MODIFIED="1371240409331" TEXT="Zmienisz zachowanie testowe"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
